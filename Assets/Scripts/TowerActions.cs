c0acd87fcbb25404765c3dc857de9ec1280834f8
Py <143103772+PyThePylon@users.noreply.github.com>
2023-09-07 13:08:29 -0700

    Tracking
    Tower now tracks, but needs to fire bullets ahead of time.

--- Assets/Scripts/TowerActions.cs
+++ Assets/Scripts/TowerActions.cs
@@ -5,33 +5,54 @@ using UnityEngine;
 public class TowerActions : MonoBehaviour
 {
 
+    [Header("Bullet Variables")]
+    public GameObject towerBullet;
+    public GameObject bulletLoc;
+
     [Header("Single Target")]
     private GameObject selectedTarget;
+    private string targetName;
 
     
     void Update()
     {
-        if(selectedTarget == null)
+        if(selectedTarget != null)
         {
-            Debug.Log("Name is missing!");
-        }
-        else
-        {
+            Vector3 targetDirection = selectedTarget.transform.position - bulletLoc.transform.position;
+
+            bulletLoc.transform.LookAt(selectedTarget.transform.position);
+
+            Vector3 frontSpawn = bulletLoc.transform.position + bulletLoc.transform.forward * 3.2f;
+
+            GameObject spawn = Instantiate(towerBullet, frontSpawn, bulletLoc.transform.rotation);
+            spawn.GetComponent<Rigidbody>().velocity += targetDirection.normalized * 10f;
+            Destroy(spawn, 4f);
             Debug.Log(selectedTarget.name);
         }
     }
 
+    IEnumerator fire()
+    {
+        yield return new WaitForSeconds(2f);
+
+
+
+
+    }
+
+
     void OnTriggerEnter(Collider other)
     {
-        if(selectedTarget == null)
+        if (other.CompareTag("EnemyCube"))
         {
             selectedTarget = other.gameObject;
+            targetName = other.gameObject.name;
         }
     }
 
     void OnTriggerExit(Collider other)
     {
-        if(selectedTarget != null)
+        if(other.CompareTag("EnemyCube") && other.gameObject.name == targetName)
         {
             selectedTarget = null;
         }
